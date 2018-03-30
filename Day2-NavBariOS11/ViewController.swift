41abfec552bee72b28ef776dd9aa83c877f614da
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-03-30 14:07:13 +0530

    First commit

--- Day2-NavBariOS11/ViewController.swift
+++ Day2-NavBariOS11/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    tableView.estimatedRowHeight = UITableViewAutomaticDimension
+    tableView.rowHeight = 101
+    
+    setupNavbar()
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func setupNavbar()  {
+    navigationController?.navigationBar.prefersLargeTitles = true
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = true
+  }
 
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    return cell
+  }
+  
 }
 
